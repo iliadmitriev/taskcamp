4b4ca1e0bad1f99f872a4ad875be199219e2d419
Ilia Dmitriev <ilia.dmitriev@gmail.com>
2021-03-18 15:24:50 +0300

    added employees app to django project with translation

--- employees/models.py
+++ employees/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Employee(models.Model):
+    firstname = models.CharField(verbose_name=_('Firstname'), max_length=50)
+    surname = models.CharField(verbose_name=_('Surname'), max_length=50)
+    email = models.EmailField(verbose_name=_('Email'), max_length=100)
+    birthdate = models.DateField(verbose_name=_('Birthdate'))
+
+    def __str__(self):
+        return f'{id}: {self.firstname} {self.surname}'
--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+from employees.models import Employee
+
+
+class Project(models.Model):
+    title = models.CharField(verbose_name=_('Title'), max_length=200)
+    description = models.CharField(verbose_name=_('Description'), max_length=2000)
+    due_date = models.DateField(verbose_name=_('Due date'), null=True)
+    is_closed = models.BooleanField(verbose_name=_('Is closed'), default=False)
+
+    def __str__(self):
+        return f'{self.id}: {self.title} ({self.is_closed})'
+
+
+class TaskStatus(models.TextChoices):
+    NEW = 'new', _('New')
+    IN_PROGRESS = 'in progress', _('In Progress')
+    DONE = 'done', _('Done')
+    CLOSED = 'closed', _('Closed')
+
+
+class Task(models.Model):
+    title = models.CharField(verbose_name=_('Title'), max_length=200)
+    description = models.CharField(verbose_name=_('Description'), max_length=2000)
+    start = models.DateTimeField(verbose_name=_('Start date'), null=True)
+    end = models.DateTimeField(verbose_name=_('End date'), null=True)
+    author = models.ForeignKey(
+        Employee, related_name='author', verbose_name=_('Author'),
+        null=True, on_delete=models.SET_NULL
+    )
+    assignee = models.ForeignKey(
+        Employee, related_name='assignee', verbose_name=_('Assignee'),
+        null=True, on_delete=models.SET_NULL
+    )
+    status = models.CharField(
+        verbose_name=_('Status'), max_length=20,
+        choices=TaskStatus.choices, default=TaskStatus.NEW
+    )
