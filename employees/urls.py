c4d222bba3eda204360fc04da80056183e0cd0f3
Ilia Dmitriev <ilia.dmitriev@gmail.com>
2021-03-19 14:18:28 +0300

    employee detail view

--- employees/urls.py
+++ employees/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import EmployeeListView
+from .views import EmployeeListView, EmployeeDetailView
 
 urlpatterns = [
-    path('', EmployeeListView.as_view(), name='employee-list')
+    path('', EmployeeListView.as_view(), name='employee-list'),
+    path('<int:pk>/', EmployeeDetailView.as_view(), name='employee-detail')
 ]
--- employees/views.py
+++ employees/views.py
@@ -1,7 +1,12 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Employee
 
 
 class EmployeeListView(ListView):
     template_name = 'employee_list.html'
+    model = Employee
+
+
+class EmployeeDetailView(DetailView):
+    template_name = 'employee_detail.html'
     model = Employee
