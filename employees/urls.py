ba7604a354949f29934cfa6c79cef453c5db9be1
Ilia Dmitriev <ilia.dmitriev@gmail.com>
2021-03-19 14:32:58 +0300

    employee delete view

--- employees/urls.py
+++ employees/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import EmployeeListView, EmployeeDetailView
+from .views import EmployeeListView, EmployeeDetailView, EmployeeDeleteView
 
 urlpatterns = [
     path('', EmployeeListView.as_view(), name='employee-list'),
-    path('<int:pk>/', EmployeeDetailView.as_view(), name='employee-detail')
+    path('<int:pk>/', EmployeeDetailView.as_view(), name='employee-detail'),
+    path('<int:pk>/delete/', EmployeeDeleteView.as_view(), name='employee-delete')
 ]
--- employees/views.py
+++ employees/views.py
@@ -1,5 +1,6 @@
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, DeleteView
 from .models import Employee
+from django.urls import reverse_lazy
 
 
 class EmployeeListView(ListView):
@@ -10,3 +11,9 @@ class EmployeeListView(ListView):
 class EmployeeDetailView(DetailView):
     template_name = 'employee_detail.html'
     model = Employee
+
+
+class EmployeeDeleteView(DeleteView):
+    template_name = 'employee_confirm_delete.html'
+    model = Employee
+    success_url = reverse_lazy('employee-list')
