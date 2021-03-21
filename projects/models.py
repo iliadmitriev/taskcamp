8343db1028c03e3728c546727991294218d4404b
Ilia Dmitriev <ilia.dmitriev@gmail.com>
2021-03-21 10:59:57 +0300

    allow blank project: due_date, description, task: start, end, author, assignee, description * model allow null, blank * fix task_class filter (end datetime is none) * fix markdown fields project and task

--- projects/models.py
+++ projects/models.py
@@ -5,8 +5,8 @@ from employees.models import Employee
 
 class Project(models.Model):
     title = models.CharField(verbose_name=_('Title'), max_length=200)
-    description = models.CharField(verbose_name=_('Description'), max_length=20000, null=True)
-    due_date = models.DateField(verbose_name=_('Due date'), null=True)
+    description = models.CharField(verbose_name=_('Description'), max_length=20000, null=True, blank=True)
+    due_date = models.DateField(verbose_name=_('Due date'), null=True, blank=True)
     is_closed = models.BooleanField(verbose_name=_('Is closed'), default=False)
 
     def __str__(self):
@@ -26,17 +26,17 @@ class TaskStatus(models.TextChoices):
 
 class Task(models.Model):
     title = models.CharField(verbose_name=_('Title'), max_length=200)
-    description = models.CharField(verbose_name=_('Description'), max_length=20000, null=True)
+    description = models.CharField(verbose_name=_('Description'), max_length=20000, null=True, blank=True)
     project = models.ForeignKey(Project, verbose_name=_('Project'), on_delete=models.CASCADE)
-    start = models.DateTimeField(verbose_name=_('Start date'), null=True)
-    end = models.DateTimeField(verbose_name=_('End date'), null=True)
+    start = models.DateTimeField(verbose_name=_('Start date'), null=True, blank=True)
+    end = models.DateTimeField(verbose_name=_('End date'), null=True, blank=True)
     author = models.ForeignKey(
         Employee, related_name='author', verbose_name=_('Author'),
-        null=True, on_delete=models.SET_NULL
+        null=True, blank=True, on_delete=models.SET_NULL
     )
     assignee = models.ForeignKey(
         Employee, related_name='assignee', verbose_name=_('Assignee'),
-        null=True, on_delete=models.SET_NULL
+        null=True, blank=True, on_delete=models.SET_NULL
     )
     status = models.CharField(
         verbose_name=_('Status'), max_length=20,
--- projects/templatetags/task_class.py
+++ projects/templatetags/task_class.py
@@ -8,9 +8,11 @@ register = template.Library()
 
 @register.filter(name='task_class', is_safe=True)
 def task_class(task):
+    if task.status == TaskStatus.CLOSED:
+        return 'table-success'
+    if task.end is None:
+        return ''
     if (task.status in [TaskStatus.NEW, TaskStatus.IN_PROGRESS]) \
             and (task.end <= timezone.now()):
         return 'table-warning'
-    if task.status == TaskStatus.CLOSED:
-        return 'table-success'
     return ''
