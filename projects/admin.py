e6d5eea9c057b9c1ebbae764b9beebf64e71ba92
Ilia Dmitriev <ilia.dmitriev@gmail.com>
2021-03-21 11:30:27 +0300

    comment admin page

--- projects/admin.py
+++ projects/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Project, Task
+from .models import Project, Task, Comment
 
 
 class ProjectAdmin(admin.ModelAdmin):
@@ -11,6 +11,7 @@ class ProjectAdmin(admin.ModelAdmin):
 
 
 class TaskAdmin(admin.ModelAdmin):
+    list_select_related = True
     readonly_fields = ['id']
     fields = [
         'id', 'project', 'title', 'description', 'start', 'end',
@@ -25,5 +26,15 @@ class TaskAdmin(admin.ModelAdmin):
     raw_id_fields = ['project', 'author', 'assignee']
 
 
+class CommentAdmin(admin.ModelAdmin):
+    list_select_related = True
+    readonly_fields = ['id', 'created']
+    fields = ['id', 'created', 'task', 'description']
+    raw_id_fields = ['task']
+    search_fields = ['description']
+    list_display = ['id', 'task', 'created', 'description']
+
+
 admin.site.register(Project, ProjectAdmin)
 admin.site.register(Task, TaskAdmin)
+admin.site.register(Comment, CommentAdmin)
