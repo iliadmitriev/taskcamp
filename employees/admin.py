a58e410546caa1dea7f454d6166a59341011857a
Ilia Dmitriev <ilia.dmitriev@gmail.com>
2021-03-22 11:10:02 +0300

    added view on site for projects, employees, tasks and comments

--- employees/admin.py
+++ employees/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.urls import reverse
 from .models import Employee
 
 
@@ -7,6 +8,9 @@ class EmployeeAdmin(admin.ModelAdmin):
     readonly_fields = ['id']
     fields = ['id', 'email', 'firstname', 'surname', 'birthdate']
     search_fields = ['email', 'firstname', 'surname']
+
+    def get_view_on_site_url(self, obj=None):
+        return reverse('employee-detail', args=(obj.id,))
 
 
 admin.site.register(Employee, EmployeeAdmin)
--- projects/admin.py
+++ projects/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.urls import reverse
 from .models import Project, Task, Comment
 
 
@@ -8,6 +9,9 @@ class ProjectAdmin(admin.ModelAdmin):
     list_display = ['id', 'title', 'description', 'due_date', 'is_closed']
     search_fields = ['title', 'description']
     list_filter = ['due_date', 'is_closed']
+
+    def get_view_on_site_url(self, obj=None):
+        return reverse('project-detail', args=(obj.id,))
 
 
 class TaskAdmin(admin.ModelAdmin):
@@ -25,6 +29,9 @@ class TaskAdmin(admin.ModelAdmin):
     list_filter = ['status']
     raw_id_fields = ['project', 'author', 'assignee']
 
+    def get_view_on_site_url(self, obj=None):
+        return reverse('projects-task-detail', args=(obj.id,))
+
 
 class CommentAdmin(admin.ModelAdmin):
     list_select_related = True
@@ -33,6 +40,9 @@ class CommentAdmin(admin.ModelAdmin):
     raw_id_fields = ['task']
     search_fields = ['description']
     list_display = ['id', 'task', 'created', 'description']
+
+    def get_view_on_site_url(self, obj=None):
+        return reverse('projects-task-detail', args=(obj.task.id,))
 
 
 admin.site.register(Project, ProjectAdmin)
