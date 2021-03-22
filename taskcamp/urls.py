1fc1f28cd4d3e9bf5e070c1808ec4d61e2ed6003
Ilia Dmitriev <ilia.dmitriev@gmail.com>
2021-03-22 10:47:15 +0300

    * added test static path to urls.py for media

--- taskcamp/urls.py
+++ taskcamp/urls.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
+
 
 urlpatterns = [
     path('', include('home.urls')),
@@ -7,3 +10,7 @@ urlpatterns = [
     path('employees/', include('employees.urls')),
     path('admin/', admin.site.urls),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
