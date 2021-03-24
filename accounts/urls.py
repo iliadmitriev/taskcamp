9d7e22a01a5d3a378e7ac9f2fbfd72a78a02db60
Ilia Dmitriev <ilia.dmitriev@gmail.com>
2021-03-24 13:37:26 +0300

    * register form * register template * register url * account register view

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include, reverse_lazy
-from .views import AccountsLoginView
+from .views import AccountsLoginView, AccountsRegisterView
 
 app_name = 'accounts'
 
 urlpatterns = [
     # path('', include('django.contrib.auth.urls')),
+    path('register/', AccountsRegisterView.as_view(), name='register'),
     path('login/', AccountsLoginView.as_view(), name='login')
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,36 @@
 from django.contrib.auth import views as auth_views
-from django.urls import reverse
+from django.contrib.auth import authenticate, login
+from django.views.generic import FormView
+from django.urls import reverse, reverse_lazy
+from django.contrib.auth import get_user_model
+
+
+from .forms import RegisterForm
+
+
+class AccountsRegisterView(FormView):
+    template_name = 'register.html'
+    form_class = RegisterForm
+    success_url = reverse_lazy('home')
+
+    def form_valid(self, form):
+
+        user_model = get_user_model()
+
+        user_model.objects.create_user(
+            form.cleaned_data.get('email'),
+            form.cleaned_data.get('password1')
+        )
+
+        user = authenticate(
+            self.request,
+            username=form.cleaned_data.get('email'),
+            password=form.cleaned_data.get('password1')
+        )
+
+        login(self.request, user)
+
+        return super().form_valid(form)
 
 
 class AccountsLoginView(auth_views.LoginView):
