fbe11b84e0430cb05383749b77b24ca888fbbd8e
Ilia Dmitriev <ilia.dmitriev@gmail.com>
2021-03-25 19:32:28 +0300

    * fix admin link * account activate url * register generate user_hash and token * account activate user by hash and token view

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,11 @@
+import uuid
+from django.core.cache import cache
+from django.conf import settings
 from django.contrib.auth import views as auth_views
 from django.contrib.auth import authenticate, login
-from django.views.generic import FormView
+from django.views.generic import FormView, View
 from django.urls import reverse, reverse_lazy
+from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth import get_user_model
 from .forms import RegisterForm
 
@@ -15,20 +19,57 @@ class AccountsRegisterView(FormView):
 
         user_model = get_user_model()
 
-        user_model.objects.create_user(
+        user = user_model.objects.create_user(
             form.cleaned_data.get('email'),
-            form.cleaned_data.get('password1')
+            form.cleaned_data.get('password1'),
+            is_active=False
         )
 
-        user = authenticate(
-            self.request,
-            username=form.cleaned_data.get('email'),
-            password=form.cleaned_data.get('password1')
+        user_hash = uuid.uuid4().hex
+        token = uuid.uuid4().hex
+
+        cache.set(
+            user_hash,
+            {
+                'token': token,
+                'user_id': user.id
+            },
+            settings.ACCOUNT_ACTIVATION_LINK_EXPIRE
         )
 
-        login(self.request, user)
+        print(reverse('accounts:activate', args=(user_hash, token,)))
 
         return super().form_valid(form)
+
+
+class AccountsRegisterActivate(View):
+
+    def get(self, *args, **kwargs):
+        user_hash = kwargs.get('user_hash')
+        token = kwargs.get('token')
+        user_model = get_user_model()
+
+        data = cache.get(user_hash)
+
+        if isinstance(data, dict) and data.get('token') == token:
+
+            try:
+                user_id = data.get('user_id')
+                user = user_model.objects.get(pk=user_id)
+
+                user.is_active = True
+                user.save()
+
+                login(self.request, user)
+
+                cache.delete(user_hash)
+
+                return HttpResponseRedirect(reverse('home'))
+
+            except user_model.DoesNotExist:
+                pass
+
+        return HttpResponse(b'hash is not found or expired')
 
 
 class AccountsLoginView(auth_views.LoginView):
