d3320c8c46bd8899aa9212f5571b4eb7226eaf3e
Ilia Dmitriev <ilia.dmitriev@gmail.com>
2021-03-31 10:24:20 +0300

    password reset send email with confirmation link

--- worker/email/tasks.py
+++ worker/email/tasks.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.core.mail import EmailMultiAlternatives
 from django.template.loader import render_to_string
+from django.template import loader
 from worker.app import app
 
 
@@ -58,3 +59,24 @@ def send_welcome_message(email, tour_link):
     message.attach_alternative(html_message, 'text/html')
 
     return message.send()
+
+
+@app.task(
+    max_retries=5,
+    default_retry_delay=60,
+    autoretry_for=(ConnectionRefusedError,)
+    )
+def send_reset_email(
+        subject_template_name, email_template_name,
+        context, from_email, to_email, html_email_template_name=None):
+    subject = loader.render_to_string(subject_template_name, context)
+    # Email subject *must not* contain newlines
+    subject = ''.join(subject.splitlines())
+    body = loader.render_to_string(email_template_name, context)
+
+    email_message = EmailMultiAlternatives(subject, body, from_email, [to_email])
+    if html_email_template_name is not None:
+        html_email = loader.render_to_string(html_email_template_name, context)
+        email_message.attach_alternative(html_email, 'text/html')
+
+    email_message.send()
