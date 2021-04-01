79d195b24b6ec82fcc6cea0e9189eaf1927faf2f
Ilia Dmitriev <ilia.dmitriev@gmail.com>
2021-04-01 13:42:26 +0300

    fix for python 3.8 support

--- accounts/templatetags/html_attributes.py
+++ accounts/templatetags/html_attributes.py
@@ -4,7 +4,8 @@ register = template.Library()
 
 @register.filter(name='add_attr')
 def add_attr(field, extra_attr):
-    attrs = field.field.widget.attrs | extra_attr
+    attrs = field.field.widget.attrs
+    attrs.update(extra_attr)
     return field.as_widget(attrs=attrs)
 
 
