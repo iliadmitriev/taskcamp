a5a751a42f406981969963cf263d7b8df588de09
Ilia Dmitriev <ilia.dmitriev@gmail.com>
2021-08-13 10:46:15 +0300

    project list view unauthorized redirect #nobuild

--- projects/tests.py
+++ projects/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase, Client
 from django.shortcuts import reverse
+from django.http import QueryDict
 
 
 class ProjectListViewTestCase(TestCase):
@@ -9,3 +10,12 @@ class ProjectListViewTestCase(TestCase):
     def test_project_list_view_unauthorized_302(self):
         response = self.client.get(reverse('project-list'))
         self.assertEqual(response.status_code, 302)
+        q = QueryDict(mutable=True)
+        q['next'] = reverse('project-list')
+        self.assertRedirects(
+            response,
+            "{0}?{1}".format(reverse('accounts:login'), q.urlencode()),
+            status_code=302,
+            target_status_code=200,
+            fetch_redirect_response=True
+        )
