118d09d1c874c54b1ec59e97056870105444b1c9
Ilia Dmitriev <ilia.dmitriev@gmail.com>
2021-08-13 10:26:34 +0300

    first test #nobuild

--- projects/tests.py
+++ projects/tests.py
@@ -1,3 +1,11 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.shortcuts import reverse
 
-# Create your tests here.
+
+class ProjectListViewTestCase(TestCase):
+    def setUp(self) -> None:
+        self.client = Client(HTTP_ACCEPT_LANGUAGE='ru')
+
+    def test_project_list_view_unauthorized_302(self):
+        response = self.client.get(reverse('project-list'))
+        self.assertEqual(response.status_code, 302)
