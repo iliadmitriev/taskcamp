eacae2e84c8d86ca864d754444cb7637b8e26694
Ilia Dmitriev <ilia.dmitriev@gmail.com>
2021-08-14 16:53:55 +0300

    Tests #nobuild * test send_reset_email

--- worker/tests/test_email.py
+++ worker/tests/test_email.py
@@ -2,7 +2,8 @@ from django.core import mail
 from django.test import TestCase
 
 from worker import celery_app
-from worker.email.tasks import send_activation_email, send_welcome_message
+from worker.email.tasks import send_activation_email, \
+    send_welcome_message, send_reset_email
 
 
 class TestMyCeleryWorker(TestCase):
@@ -21,3 +22,27 @@ class TestMyCeleryWorker(TestCase):
         self.assertEqual(len(mail.outbox), 1)
         self.assertEqual(mail.outbox[0].subject, 'Taskcamp welcomes you')
         self.assertEqual(res, 1)
+
+    def send_reset_email_success(self, html=True):
+        send_reset_email(
+            subject_template_name='email/password_reset_email_subj.txt',
+            email_template_name='email/password_reset_email.html',
+            context={
+                'protocol': 'https',
+                'domain': 'example.com',
+                'uid': 'uidb64',
+                'token': 'token',
+                'user': {'get_username': 'name'}
+            },
+            from_email='from@example.com',
+            to_email='to@example.com',
+            html_email_template_name='email/password_reset_email_html.html' if html else None
+        )
+        self.assertEqual(len(mail.outbox), 1)
+        self.assertEqual(mail.outbox[0].subject, 'Reset password')
+
+    def test_send_reset_email_html(self):
+        self.send_reset_email_success(html=True)
+
+    def test_send_reset_email_not_html(self):
+        self.send_reset_email_success(html=False)
