ac2f0f096a6a26c0ce729f2503a78a7c7143fa72
Ilia Dmitriev <ilia.dmitriev@gmail.com>
2021-08-15 01:19:13 +0300

    Tests #nobuild * project, task model tests

---
+++ projects/tests/test_models.py
@@ -0,0 +1,20 @@
+from django.test import TestCase
+from projects.models import Project, Task
+
+
+class ProjectsTestCase(TestCase):
+    def setUp(self) -> None:
+        self.project = Project.objects.create(title='Test project')
+        self.task = Task.objects.create(title='Test task', project=self.project)
+
+    def test_project_model_str(self):
+        self.assertEqual(
+            self.project.__str__(),
+            f'{self.project.id}: {self.project.title} ({self.project.is_closed})'
+        )
+
+    def test_task_model_str(self):
+        self.assertEqual(
+            self.task.__str__(),
+            f'{self.task.id}: {self.task.title} ({self.task.status})'
+        )
