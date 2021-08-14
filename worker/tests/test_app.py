3fb51b6cf6172e370100cd4656aa63c549a459ce
Ilia Dmitriev <ilia.dmitriev@gmail.com>
2021-08-14 22:55:27 +0300

    Tests #nobuild * worker app import test

---
+++ worker/tests/test_app.py
@@ -0,0 +1,27 @@
+import os
+from unittest import mock
+
+from django.test import TestCase
+import importlib
+from worker import app
+
+
+class WorkerAppTestCase(TestCase):
+    def setUp(self) -> None:
+        os.environ.clear()
+
+    def test_worker_app_init(self):
+        with mock.patch.dict('os.environ', {
+            'RABBITMQ_HOST': 'rabbitmq',
+            'RABBITMQ_PORT': '5672',
+            'RABBITMQ_DEFAULT_VHOST': 'celery',
+            'RABBITMQ_DEFAULT_USER': 'celery',
+            'RABBITMQ_DEFAULT_PASS': 'password'
+        }):
+            self.assertIsNotNone(os.environ.get('RABBITMQ_HOST'))
+            importlib.reload(app)
+            self.assertEqual(app.broker_url, 'pyamqp://celery:password@rabbitmq:5672/celery')
+
+        self.assertIsNone(os.environ.get('RABBITMQ_HOST'))
+        importlib.reload(app)
+        self.assertIsNone(app.broker_url)
