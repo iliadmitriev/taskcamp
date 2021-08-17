7f1825f455d7fa2babef3608681ccda51ddb7fd2
Ilia Dmitriev <ilia.dmitriev@gmail.com>
2021-08-17 11:17:11 +0300

    Tests * test 403, 404

--- taskcamp/tests/test_views.py
+++ taskcamp/tests/test_views.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from taskcamp.views import http_500
+from taskcamp.views import http_500, http_404, http_403
 
 
 class HandlingViewsTestCase(TestCase):
@@ -7,3 +7,13 @@ class HandlingViewsTestCase(TestCase):
         response = http_500(request=None)
         self.assertEqual(response.status_code, 500)
         self.assertTemplateUsed('handler/500.html')
+
+    def test_404_page_handler(self):
+        response = http_404(request=None, exception=Exception)
+        self.assertEqual(response.status_code, 404)
+        self.assertTemplateUsed('handler/404.html')
+
+    def test_403_page_handler(self):
+        response = http_403(request=None, exception=Exception)
+        self.assertEqual(response.status_code, 403)
+        self.assertTemplateUsed('handler/403.html')
